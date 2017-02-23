00c5b5af9edbcf5f2aadddc9abd4dcd7dd4b0984
Muhammad Waqas <waqasazamkml@gmail.com>
2017-02-23 18:02:26 +0500

    Designed New Activity. Activity moving using Intents.

--- app/src/main/java/com/lecture3/MainActivity.java
+++ app/src/main/java/com/lecture3/MainActivity.java
@@ -1,13 +1,29 @@
 package com.lecture3;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
