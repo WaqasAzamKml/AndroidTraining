78d009324cdf5cd3db1f056460909a7c820d9a06
Muhammad Waqas <waqasazamkml@gmail.com>
2017-03-08 18:17:24 +0500

    Added Spinner in Signup. Added inputTpe in EditText objects.

--- app/src/main/java/com/lecture3/SignupActivity.java
+++ app/src/main/java/com/lecture3/SignupActivity.java
@@ -2,12 +2,24 @@ package com.lecture3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SignupActivity extends AppCompatActivity {
+
+    Spinner spnGender, spnBloodGroup;
+    String[] genderArray = {"Male", "Female", "Other"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        spnGender = (Spinner) findViewById(R.id.spnGender);
+        spnBloodGroup = (Spinner) findViewById(R.id.spnBloodGroup);
+
+        ArrayAdapter<String> genderAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, genderArray);
+        spnGender.setAdapter(genderAdapter);
+
     }
 }
